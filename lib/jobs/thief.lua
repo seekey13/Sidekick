da1809f87a9dea34630eef8bccd15f1c62f3a03e
Seekey13 <dcichy@gmail.com>
2026-07-10 18:15:48 -1000

    Remove obsolete Thief abilities: Steal, Mug, Bully, and Despoil from job definition

--- lib/jobs/thief.lua
+++ lib/jobs/thief.lua
@@ -13,48 +13,12 @@ return {
     abilities = {
         buff = {
             {
-                name = "Steal",
-                level = 5,
-                cost = 0,
-                id = 60,
-                range = 3,
-                command = '/ja "Steal" <bt>',
-                combat_only = true,
-            },
-            {
-                name = "Mug",
-                level = 35,
-                cost = 0,
-                id = 65,
-                range = 3,
-                command = '/ja "Mug" <bt>',
-                combat_only = true,
-            },
-            {
-                name = "Bully",
-                level = 60,
-                cost = 0,
-                id = 240,
-                range = 3,
-                command = '/ja "Bully" <bt>',
-                combat_only = true,
-            },
-            {
                 name = "Conspirator",
                 level = 75,
                 cost = 0,
                 id = 40,
                 command = '/ja "Conspirator" <me>',
                 buff_id = 462,
-                combat_only = true,
-            },
-            {
-                name = "Despoil",
-                level = 60,
-                cost = 0,
-                id = 61,
-                range = 3,
-                command = '/ja "Despoil" <bt>',
                 combat_only = true,
             },
             {
